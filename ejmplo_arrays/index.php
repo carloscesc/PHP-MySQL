c6134138de672dfc6bfa1723617fa5f52a689a65
Carlos Raudales <cjraudales@hotmail.com>
2018-10-01 14:21:17 -0600

    Array indexados, asociativos y multidimensionales

--- ejmplo_arrays/index.php
+++ ejmplo_arrays/index.php
@@ -6,7 +6,7 @@
 	       	<meta name="viewport" content="width=device-width, initial-scale=1">
 	       	<link href="css/bootstrap.min.css" rel="stylesheet"> 
 		
-		<title>Ejemplo de Array</title>
+		<title>Ejemplo de Arrays</title>
 		
 	</head>
 	<body>
